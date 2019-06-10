88739a4f75592c834ba9c39548e8daa13823834b
Adam Cox <mr.adamcox@gmail.com>
2019-06-10 22:48:16 +0000

    update map settings

--- eamena/eamena/settings.py
+++ eamena/eamena/settings.py
@@ -154,6 +154,15 @@ def RESOURCE_TYPE_CONFIGS():
     }
 
 
+# Map settings
+DEFAULT_MAP_X = 3000000
+DEFAULT_MAP_Y = 2200000
+DEFAULT_MAP_ZOOM = 4
+MAP_MAX_ZOOM = 19
+MAP_MAX_UNLOGGED_ZOOM = 10 # This is the max level of zoom for anonymous users
+REPORT_MIN_UNLOGGED_ZOOM = 16
+ENCODING_KEY = None
+
 #The list of resource types which should have their geometries indexed into the maplayers index for restriction by geo area.
 MAPLAYERS_INDEX_RESOURCE_TYPES = [
     "HERITAGE_RESOURCE_GROUP.E27",
