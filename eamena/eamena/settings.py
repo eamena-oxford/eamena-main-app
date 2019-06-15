b55876684ca17ea692ebcbd2c829dd38684f8168
adam cox <mr.adamcox@gmail.com>
2019-06-15 15:54:21 -0500

    update rdm contents

--- eamena/eamena/settings.py
+++ eamena/eamena/settings.py
@@ -176,7 +176,7 @@ ADDITIONAL_RESOURCE_GRAPH_LOCATIONS = (
 )
 
 SKOS_FILE_LOCATION = os.path.join(PACKAGE_ROOT,"source_data","concepts",
-    "EAMENA-main-app-RDM-June10.xml")
+    "EAMENA-main-app-RDM-June11.xml")
 
 CONCEPT_SCHEME_LOCATIONS = (
     # Put strings here, like "/home/data/authority_files" or "C:/data/authority_files".
